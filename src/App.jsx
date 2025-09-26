52911aadc07dd7d6f89dcaadbf5f1857e907f05b
nicklefritz <nrcoover@gmail.com>
2025-09-26 17:51:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
-  return (
-    <>
-      <Player />
-      <div id="challenges"></div>
-    </>
-  );
+	return (
+		<>
+			<Player />
+			<div id="challenges">
+				<TimerChallenge title="Easy" targetTime={1} />
+				<TimerChallenge title="Normal" targetTime={5} />
+				<TimerChallenge title="Intermediate" targetTime={10} />
+				<TimerChallenge title="Hard" targetTime={15} />
+			</div>
+		</>
+	);
 }
 
 export default App;
